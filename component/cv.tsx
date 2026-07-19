06d7d9790813a5b83a43ebecf09f5db5226566ca
mhar <romar.bucad@airlinemetrics.com>
2026-07-19 12:35:11 +0800

    add CV download link to home page and responsive

--- component/cv.tsx
+++ component/cv.tsx
@@ -1,7 +1,7 @@
 export default function Cv() {
     return <>
 
-        <a href="/CV.pdf" title="Download CV" download className="px-4 py-4 bg-blue-600 text-white rounded" >
+        <a href="/CV.pdf" title="Download CV" download className="px-4 py-4 bg-blue-500 text-white rounded" >
             Download CV
         </a>
     </>;
